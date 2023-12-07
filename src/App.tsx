1ea906754cdf2c4d4d39c909324f072eeaa8284c
Satyam Saurabh <ss9125rpvv@gmail.com>
2023-12-07 17:40:29 +0530

    add debug statment

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Home from "./modules/home";
 
 const App = () => {
+  console.log('Server running at: ', import.meta.env.VITE_GRAPHQL_SERVER_URL);
   const client = new ApolloClient({
     uri: `${import.meta.env.VITE_GRAPHQL_SERVER_URL}/graphql`,
     cache: new InMemoryCache(),
